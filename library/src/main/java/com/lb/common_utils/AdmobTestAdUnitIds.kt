bc238ad48c1d91211e2e5a4876a29a52f4850faa
AndroidDeveloperLB <lblb636@gmail.com>
2024-08-12 15:46:30 +0300

    Added Admob test ids

---
+++ library/src/main/java/com/lb/common_utils/AdmobTestAdUnitIds.kt
@@ -0,0 +1,36 @@
+package com.lb.common_utils
+
+//https://developers.google.com/admob/android/test-ads
+object AdmobTestAdUnitIds {
+    /**https://developers.google.com/admob/android/app-open#always_test_with_test_ads*/
+    const val APP_OPEN = "ca-app-pub-3940256099942544/9257395921"
+
+    /**https://developers.google.com/admob/android/banner#always_test_with_test_ads*/
+    const val BANNER_ADAPTIVE = "ca-app-pub-3940256099942544/9214589741"
+
+    /**https://developers.google.com/admob/android/banner/fixed-size#add-adview*/
+    const val BANNER = "ca-app-pub-3940256099942544/6300978111"
+
+    /** https://developers.google.com/admob/android/banner/inline-adaptive https://github.com/googleads/googleads-mobile-ios-examples/blob/6def5ddd3f4b0b92b9fc8fec3e88e011c21f36b5/Swift/admob/AdaptiveBannerExample/AdaptiveBannerExample/ViewController.swift#LL28C28-L28C66    https://groups.google.com/g/google-admob-ads-sdk/c/sENzJ0kycfE */
+    const val BANNER_INLINE_ADAPTIVE_ = "ca-app-pub-3940256099942544/2435281174"
+
+    /**https://developers.google.com/admob/android/interstitial#always_test_with_test_ads*/
+    const val INTERSTITIAL = "ca-app-pub-3940256099942544/1033173712"
+
+    const val INTERSTITIAL_VIDEO = "ca-app-pub-3940256099942544/8691691433"
+
+    /**https://developers.google.com/admob/android/rewarded-interstitial#load_an_ad_2*/
+    const val INTERSTITIAL_REWARDED = "ca-app-pub-3940256099942544/5354046379"
+
+    /**https://developers.google.com/admob/android/rewarded#always_test_with_test_ads*/
+    const val REWARDED = "ca-app-pub-3940256099942544/5224354917"
+
+    /**https://developers.google.com/admob/android/native#always_test_with_test_ads_2*/
+    const val NATIVE_IMAGE = "ca-app-pub-3940256099942544/2247696110"
+
+    /**https://developers.google.com/admob/android/native/video-ads*/
+    const val NATIVE_IMAGE_AND_VIDEO = "ca-app-pub-3940256099942544/1044960115"
+
+    /** https://developers.google.com/admob/android/native/full-screen https://groups.google.com/g/google-admob-ads-sdk/c/sREXDkNNYuU https://www.google.com/url?sa=j&url=https%3A%2F%2Fgithub.com%2Fgoogleads%2Fgoogleads-mobile-android-examples%2Fblob%2F7829ae98c4925d547c149c6aa71a25930adb79d2%2Fjava%2Fadmob%2FFullScreenNativeExample%2Fapp%2Fsrc%2Fmain%2Fjava%2Fcom%2Fgoogle%2Fexample%2Fgms%2Ffullscreennativeexample%2FNativeAdsPool.java%23L48&uct=1705091050&usg=lxSuTFHWRfv6H28_h5mIwMe5WT8.&opi=112976253*/
+    const val NATIVE_FULL_SCREEN = "ca-app-pub-3940256099942544/7342230711"
+}
