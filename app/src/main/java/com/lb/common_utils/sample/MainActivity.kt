015c7b95b61f0a54d71884cbf4a6243aca501062
AndroidDeveloperLB <lblb636@gmail.com>
2026-02-19 01:29:09 +0200

    Updated sample

--- app/src/main/java/com/lb/common_utils/sample/MainActivity.kt
+++ app/src/main/java/com/lb/common_utils/sample/MainActivity.kt
@@ -1,34 +1,66 @@
 package com.lb.common_utils.sample
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.net.toUri
+import androidx.core.view.MenuProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
+import androidx.core.view.updatePadding
+import com.lb.common_utils.BoundActivity
 import com.lb.common_utils.sample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity: BoundActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(0))
+        ViewCompat.setOnApplyWindowInsetsListener(binding.appBarLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+            binding.appBarLayout.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
         }
+        setSupportActionBar(binding.toolbar)
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menu.addSubMenu("More info").let { subMenu ->
+                    subMenu.setIcon(android.R.drawable.ic_menu_info_details)
+                    subMenu.item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+                    subMenu.add("Repository website").setOnMenuItemClickListener(
+                        createUrlMenuItemClickListener("https://github.com/AndroidDeveloperLB/CommonUtils")
+                    )
+                    subMenu.add("All my repositories").setOnMenuItemClickListener(
+                        createUrlMenuItemClickListener("https://github.com/AndroidDeveloperLB")
+                    )
+                    subMenu.add("All my apps").setOnMenuItemClickListener(
+                        createUrlMenuItemClickListener("https://play.google.com/store/apps/developer?id=AndroidDeveloperLB")
+                    )
+                }
+            }
+
+            private fun createUrlMenuItemClickListener(url: String): MenuItem.OnMenuItemClickListener {
+                return MenuItem.OnMenuItemClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+                    @Suppress("DEPRECATION")
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                    startActivity(intent)
+                    true
+                }
+            }
+
+            override fun onMenuItemSelected(item: MenuItem): Boolean {
+                return true
+            }
+        })
     }
 }
