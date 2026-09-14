892005925e19cd0c0d51665fd2b3e7b843d2ed09
AndroidDeveloperLB <lblb636@gmail.com>
2026-09-14 03:36:56 +0300

    Added MediatorLiveData2 similar to MutableLiveData2, producing only non-null values.

---
+++ library/src/main/java/MediatorLiveData2.kt
@@ -0,0 +1,27 @@
+import androidx.annotation.AnyThread
+import androidx.annotation.UiThread
+import androidx.lifecycle.MediatorLiveData
+import com.lb.common_utils.MutableLiveData2
+
+fun <T, R> MutableLiveData2<T>.mapNonNull(transform: (T) -> R): MediatorLiveData2<R> {
+    val result = MediatorLiveData2(transform(getValue()))
+    result.addSource(this) { x ->
+        result.value = transform(x)
+    }
+    return result
+}
+
+@Suppress("UNCHECKED_CAST")
+open class MediatorLiveData2<T>(initialValue: T) : MediatorLiveData<T>() {
+    init {
+        value = initialValue
+    }
+
+    override fun getValue(): T = super.getValue() as T
+
+    @UiThread
+    override fun setValue(value: T) = super.setValue(value)
+
+    @AnyThread
+    override fun postValue(value: T) = super.postValue(value)
+}
